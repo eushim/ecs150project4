ca977d6b72d9bbec26440d30b112bcc031710a25
Joshua Heideman <joshuacheideman@gmail.com>
2017-05-24 05:26:01 +0000

    Did part of part 1

--- libfs/fs.c
+++ libfs/fs.c
@@ -2,39 +2,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "disk.h"
 #include "fs.h"
 
-struct superblock{
-	int num_blocks;
-	int fat_size;
+struct __attribute__((__packed__)) superblock{
+	uint64_t signature;
+	uint16_t total_amount;
+	uint16_t root_index;
+	uint16_t data_index;
+	uint16_t num_data;
+	uint8_t num_FAT;
 };
 
 
-struct file_allocation_table{
-	void * arr;	//data blocks
+
+
+struct __attribute__((__packed__)) rootdirectory{
+	void * filename;
+	uint32_t file_size;
+	uint16_t first_index;
 };
 
-
-struct root{
+struct __attribute__((__packed__)) FAT{
 	void * arr;
-};
-
-struct file{
-	char * name;
-	int size;
-	void * location;
 };
 
 
 int fs_mount(const char *diskname)
 {
+	void * superbuf = NULL;
+	if(block_disk_open(diskname)==-1)
+		return -1;
+	block_read(0,superbuf);
+	struct superblock * super= (struct superblock *)malloc(sizeof(struct superblock));
+	
+	memcpy((void*)super->signature,superbuf,8);
+	
+	printf("SUPER %c", super->signature);
+	if(super->signature!="ECS150FS")
+		return -1;
 	return 0;
 }
 
 int fs_umount(void)
 {
+	block_disk_close();
+	
 	return 0;
 }
 
@@ -45,11 +60,17 @@ int fs_info(void)
 
 int fs_create(const char *filename)
 {
+	if (filename == NULL)
+		return -1;
+		
 	return 0;
 }
 
 int fs_delete(const char *filename)
 {
+	if (filename == NULL)
+		return -1;
+	
 	return 0;
 }
 
@@ -60,6 +81,9 @@ int fs_ls(void)
 
 int fs_open(const char *filename)
 {
+	if (filename == NULL)
+		return -1;
+		
 	return 0;
 }
 
